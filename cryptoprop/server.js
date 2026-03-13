a5ca4acfcee2e455efe8586a521b8010711e23fc
fryzack25-gif <fryzack25@gmail.com>
2026-03-12 20:12:59 -0400

    Update server.js

--- cryptoprop/server.js
+++ cryptoprop/server.js
@@ -166,7 +166,7 @@ app.use(session({
   saveUninitialized: true,
   cookie: {
     httpOnly: true,
-    sameSite: "lax",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
     secure: (process.env.NODE_ENV === "production"),
     maxAge: 1000 * 60 * 60 * 24 * 14
   }
